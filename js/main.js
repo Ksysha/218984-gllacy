70cdcab31f030666973b2259671453f1ea7caae4
Ксения Цвиркун <ksysha149@gmail.com>
2016-11-20 21:49:14 +0300

    ENd

--- js/main.js
+++ js/main.js
@@ -1,33 +1,52 @@
 var link = document.querySelector(".feedback");
 
-var feedbackForm = $(".popup-feedback");
-var feedbackFormOverlay = $(".popup-feedback-overlay");
-var close = feedbackForm.find(".exit");
-var nameFeedback = feedbackForm.find("input[name=feedback-name]");
-var emailFeedback = feedbackForm.find("input[name=feedback-email]");
-var message = feedbackForm.find("textarea[name=user-message]");
+var feedbackForm = document.querySelector(".popup-feedback");
+var close = feedbackForm.querySelector(".exit");
+var feedbackFormOverlay = document.querySelector(".popup-feedback-overlay");
+var nameFeedback = feedbackForm.querySelector("[name=feedback-name]");
+var emailFeedback = feedbackForm.querySelector("[name=feedback-email]");
+var nameFeedbackLabel = feedbackForm.querySelector("[for=feedback-name]");
+var emailFeedbackLabel = feedbackForm.querySelector("[for=feedback-email]");
+var message = feedbackForm.querySelector("[name=user-message]");
+var storageEmail = localStorage.getItem("email");
+var storageName = localStorage.getItem("name");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
 
-  feedbackForm.addClass("popup-show");
-  feedbackFormOverlay.addClass("popup-show");
+  feedbackForm.classList.add("popup-show");
+  feedbackFormOverlay.classList.add("popup-show");
 
-  feedbackForm.find('input, textarea').filter(function() {
-    return this.value === '';
-  }).first().focus();
+  if (storageName && storageEmail) {
+    nameFeedback.value = storageName;
+    emailFeedback.value = storageEmail;
+    emailFeedbackLabel.classList.add("change-label");
+    nameFeedbackLabel.classList.add("change-label");
+    message.focus();
+  } else {
+    if (storageName) {
+      nameFeedback.value = storageName;
+      nameFeedbackLabel.classList.add("change-label");
+      emailFeedback.focus();
+    }
+     else {
+      emailFeedback.value = storageEmail;
+      emailFeedbackLabel.classList.add("change-label");
+      nameFeedback.focus();
+    }
+  }
 });
 
-close.on("click", function(event) {
+close.addEventListener("click", function(event) {
   event.preventDefault();
-  feedbackForm.removeClass("popup-show popup-feedback-error");
-  feedbackFormOverlay.removeClass("popup-show");
+  feedbackForm.classList.remove("popup-show", "popup-feedback-error");
+  feedbackFormOverlay.classList.remove("popup-show");
 });
 
-feedbackForm.on("submit", function(event) {
-  if (!nameFeedback.val() || !emailFeedback.val() || !message.val()) {
+feedbackForm.addEventListener("submit", function(event) {
+  if (!nameFeedback.value || !emailFeedback.value || !message.value) {
     event.preventDefault();
-    feedbackForm.addClass("popup-feedback-error");
+    feedbackForm.classList.add("popup-feedback-error");
   } else {
     localStorage.setItem("email", emailFeedback.value);
     localStorage.setItem("name", nameFeedback.value);
@@ -36,28 +55,30 @@ feedbackForm.on("submit", function(event) {
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-    if (feedbackForm.hasClass("popup-show")) {
-      feedbackForm.removeClass("popup-show", "popup-feedback-error");
-      feedbackFormOverlay.removeClass("popup-show");
+    if (popupFeedback.classList.contains("popup-feedback-show")) {
+      feedbackForm.classList.remove("popup-feedback-show", "popup-feedback-error");
+      feedbackFormOverlay.classList.remove("popup-show");
+      feedbackForm.offsetWidth = feedbackForm.offsetWidth;
     }
   }
 });
 
-var storageEmail = localStorage.getItem("email");
-var storageName = localStorage.getItem("name");
-
-nameFeedback.val(storageName);
-emailFeedback.val(storageEmail);
-
-var feedbackInputs = $(feedbackForm).find("input");
-feedbackInputs.on("change", function() {
-  var label = $(this).next('label');
-  if (this.value != '') {
-    label.addClass("change-label");
+function onchageName() {
+  if (nameFeedback.value != '') {
+    nameFeedbackLabel.classList.add("change-label");
   } else {
-    label.removeClass("change-label");
+    nameFeedbackLabel.classList.remove("change-label");
   }
-});
-$.each(feedbackInputs, function(i, element) {
-  $(element).triggerHandler("change");
-})
+};
+
+function onchageEmail() {
+  if (emailFeedback.value != '') {
+    emailFeedbackLabel.classList.add("change-label");
+  } else {
+    emailFeedbackLabel.classList.remove("change-label");
+  }
+};
+
+nameFeedback.addEventListener("change", onchageName);
+
+emailFeedback.addEventListener("change", onchageEmail);
